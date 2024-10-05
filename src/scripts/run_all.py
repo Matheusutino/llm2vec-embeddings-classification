174ed0942b26df17249f60d8968833fe3a2b75dc
Matheus Utino <matheusutino@gmail.com>
2024-10-05 18:41:00 -0300

    try catch run_all

--- src/scripts/run_all.py
+++ src/scripts/run_all.py
@@ -3,28 +3,26 @@ from src.core.utils import read_json, get_all_files_in_directory
 from src.scripts.run_classificator import run_classificator
 
 def run_all_models(models_path: str, datasets_path: str, cv: int, instruction: str):
-    # Carregar os modelos do arquivo JSON
     models = read_json(models_path)
     datasets = get_all_files_in_directory(datasets_path)
 
-    print(datasets)
-
-    # Iterar sobre os modelos de cada tipo e chamar a função run_classificator
     for dataset in datasets:
         for embedding_type, model_list in models.items():
             for model in model_list:
-                print(f"Running model for embedding type: {embedding_type}")
-                if embedding_type == 'bert':
-                    model_name = model['model_name']
-                    run_classificator(dataset, embedding_type, cv=cv, model_name=model_name)
-                elif embedding_type == 'llama_cpp':
-                    repo_id = model['repo_id']
-                    filename = model['filename']
-                    run_classificator(dataset, embedding_type, cv=cv, repo_id=repo_id, filename=filename)
-                elif embedding_type == 'llm2vec':
-                    model_base_name = model['model_base_name']
-                    model_name_version = model['model_name_version']
-                    run_classificator(dataset, embedding_type, cv=cv, model_base_name=model_base_name, model_name_version=model_name_version, instruction=instruction)
+                try:
+                    if embedding_type == 'bert':
+                        model_name = model['model_name']
+                        run_classificator(dataset, embedding_type, cv=cv, model_name=model_name)
+                    elif embedding_type == 'llama_cpp':
+                        repo_id = model['repo_id']
+                        filename = model['filename']
+                        run_classificator(dataset, embedding_type, cv=cv, repo_id=repo_id, filename=filename)
+                    elif embedding_type == 'llm2vec':
+                        model_base_name = model['model_base_name']
+                        model_name_version = model['model_name_version']
+                        run_classificator(dataset, embedding_type, cv=cv, model_base_name=model_base_name, model_name_version=model_name_version, instruction=instruction)
+                except Exception as e:
+                    print(f"Error : {e}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some parameters for model training.")
