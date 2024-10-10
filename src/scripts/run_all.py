06403d83ebfae9502d99ce0e0754ac11c585ef50
Matheus Utino <matheusutino@gmail.com>
2024-10-10 07:46:04 -0300

    change run_all.py

--- src/scripts/run_all.py
+++ src/scripts/run_all.py
@@ -2,7 +2,7 @@ import argparse
 from src.core.utils import read_json, get_all_files_in_directory
 from src.scripts.run_classificator import run_classificator
 
-def run_all_models(models_path: str, datasets_path: str, cv: int, instruction: str):
+def run_all_models(models_path: str, datasets_path: str, model_classifier:str, cv: int, instruction: str):
     models = read_json(models_path)
     datasets = get_all_files_in_directory(datasets_path)
 
@@ -12,15 +12,15 @@ def run_all_models(models_path: str, datasets_path: str, cv: int, instruction: s
                 try:
                     if embedding_type == 'bert':
                         model_name = model['model_name']
-                        run_classificator(dataset, embedding_type, cv=cv, model_name=model_name)
+                        run_classificator(dataset, embedding_type, model_classifier, cv=cv, model_name=model_name)
                     elif embedding_type == 'llama_cpp':
                         repo_id = model['repo_id']
                         filename = model['filename']
-                        run_classificator(dataset, embedding_type, cv=cv, repo_id=repo_id, filename=filename)
+                        run_classificator(dataset, embedding_type, model_classifier, cv=cv, repo_id=repo_id, filename=filename)
                     elif embedding_type == 'llm2vec':
                         model_base_name = model['model_base_name']
                         model_name_version = model['model_name_version']
-                        run_classificator(dataset, embedding_type, cv=cv, model_base_name=model_base_name, model_name_version=model_name_version, instruction=instruction)
+                        run_classificator(dataset, embedding_type, model_classifier, cv=cv, model_base_name=model_base_name, model_name_version=model_name_version, instruction=instruction)
                 except Exception as e:
                     print(f"Error : {e}")
 
@@ -29,10 +29,11 @@ if __name__ == "__main__":
     
     parser.add_argument("--models_path", type=str, default= "configs/models_infos.json", help="Path to the JSON file containing model configurations.")
     parser.add_argument("--datasets_path", type=str, default= "datasets", help="Path to the dataset CSV file.")
+    parser.add_argument("--model_classifier", type=str,  default="knn", help="Model classifier name.")
     parser.add_argument("--cv", type=int, default=5, help="Number of cross-validation folds (default: 5).")
     parser.add_argument("--instruction", type=str, default = "Summarize and capture the main points of the text.", help="Instruction for LLM2Vec embeddings.", required=False)
     
     args = parser.parse_args()
 
     # Chamada do run_all_models com os parâmetros fornecidos
-    run_all_models(args.models_path, args.datasets_path, cv=args.cv, instruction=args.instruction)
+    run_all_models(args.models_path, args.datasets_path, args.model_classifier, cv=args.cv, instruction=args.instruction)
